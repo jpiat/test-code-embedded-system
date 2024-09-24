49c2d9e114b5abaece67d78321d40105d2a98cfa
Jonathan Piat <jonathan@wsystem.com>
2024-09-24 16:33:16 +0200

    Added environment to source

--- env.sh
+++ env.sh
@@ -1,5 +1,3 @@
 source poky/oe-init-build-env
-cd ..
-#source poky/buildtools/environment-setup-x86_64-pokysdk-linux
 export MACHINE=raspberrypi4-64
-export DISTRO=core-image-base
+export DISTRO=waves
