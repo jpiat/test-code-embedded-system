026a14b84bc18cfbc840c5af49f6113d77bf21d2
Jonathan Piat <jonathan@wsystem.com>
2024-09-24 16:32:21 +0200

    Added submodules and initial yocto layer configuration

---
+++ bootstrap.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+TOPDIR=$(pwd)
+
+# This script can be use to create the build environment
+
+function exit_error() {
+    echo "Error: $1"
+    exit 1
+}
+
+# Clone all needed repository
+YOCTO_BRANCH=scarthgap
+git submodule init
+git submodule update
+
+# Install build tools
+source ${TOPDIR}/poky/oe-init-build-env ||exit_error "failed to source build environment"
+${TOPDIR}/poky/scripts/install-buildtools || exit_error "failed to install build tools"
---
+++ env.sh
@@ -0,0 +1,5 @@
+source poky/oe-init-build-env
+cd ..
+#source poky/buildtools/environment-setup-x86_64-pokysdk-linux
+export MACHINE=raspberrypi4-64
+export DISTRO=core-image-base
